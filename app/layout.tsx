1145b5cb0ca5bc074aeb77f7df12ef8c4b90e4d9
David Deng <david.j.deng@rutgers.edu>
2025-02-28 23:27:19 -0500

    added insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights/>
         <nav className="p-4">
           <div className="flex gap-6">
             {navItems.map((item) => (
